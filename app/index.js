52a839294a249e63b2de48a8bdaca6a138a64908
mayankamencherla <mayankamencherla24@gmail.com>
2018-08-14 01:09:29 +0530

    [wallet] Added apis to add and view transactions in transaction pool in the wallet instance

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
 const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
 
 app.use(bodyParser.json());
 
@@ -23,6 +27,16 @@ app.post('/mine', (req, res) => {
   p2pServer.syncChains();
 
   res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+  const {recipient, amount} = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  res.redirect('/transactions');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));

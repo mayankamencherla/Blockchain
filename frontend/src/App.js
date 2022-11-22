23b4f549e705e5fed3e0c309689cbb153c3ad473
J-coder13 <66383381+J-coder13@users.noreply.github.com>
2022-11-22 02:27:09 -0500

    Link Front and backend
    Link front end and backend utilizing react.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState}from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    const [publicKeyData, setPublicKeyData] = useState([{}])
+    const [balanceData, setBalanceData] = useState([{}])
+
+
+    useEffect(() => {
+        fetch("/public-key").then(response => response.json())
+        .then(data => {setPublicKeyData(data)})
+    }, [])
+    
+
+    useEffect(() => {
+        fetch("/balance").then(response => response.json())
+        .then(data => {setBalanceData(data)})
+    }, [])
+    
+    return (
+    <div>
+        <div>
+        {(typeof balanceData.balance=== "undefined") ? (
+            <p>Loading...</p>
+        ):(
+            balanceData.balance.map((availableCash) => (
+                <p key={0}>Your available datacoin is {availableCash}</p>
+            ))
+        )}
+        </div>
+        <div>
+        {(typeof publicKeyData.publicKey== "undefined") ? (
+            <p>Loading...</p>
+        ):(
+            publicKeyData.publicKey.map((publicKey) => (
+                <p key={0}>Your public key : {publicKey}</p>
+            ))
+        )}
+        </div>
     </div>
-  );
+
+    )
 }
 
-export default App;
+export default App

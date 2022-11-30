edc73bedeed828ea64f3cd8b1a9adc650955ceb5
Can Dilikoglu <55206720+candilikoglu@users.noreply.github.com>
2022-11-30 03:50:20 -0500

    MongoDB login and register REST API

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const nodeRSA = require('node-rsa');
+const ChainUtil = require('../chain-util');
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');
 const P2pServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
 const Miner = require('./miner');
+const dbCoonect = require('../db/dbConnect');
+const User = require('../db/userModel');
+const { PBKDF2 } = require('crypto-js');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -14,10 +22,10 @@ const bc = new Blockchain();
 const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc, tp);
 const wallet = new Wallet();
+const key = new nodeRSA({b : 1024})
 
 // A miner consisting of the local blockchain, transaction pool, currency wallet and the p2pServer
 const miner = new Miner(bc, tp, wallet, p2pServer);
-const dbCoonect = require('../db/dbConnect');
 dbCoonect()
 
 app.use(bodyParser.json());
@@ -74,5 +82,107 @@ app.post('/mine', (req, res) => {
   res.redirect('/blocks');
 });
 
-app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
-p2pServer.listen();
+
+// login endpoint
+app.post("/login", (request, response) => {
+  // check if username exists
+  User.findOne({ username: request.body.username })
+
+    // if username exists
+    .then((user) => {
+      // compare the passphrase entered and the hashed passphrase found
+      bcrypt
+        .compare(request.body.passphrase, user.passphrase)
+
+        // if the passwords match
+        .then((passphraseCheck) => {
+
+          // check if password matches
+          if(!passphraseCheck) {
+            return response.status(400).send({
+              message: "Passphrase does not match",
+              error,
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              username: user.username,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          //   return success response
+          response.status(200).send({
+            message: "Login Successful",
+            username: user.username,
+            passphrase: user.passphrase,
+            publicKey: user.publicKey.toString(),
+            token,
+          });
+        })
+        // catch error if password does not match
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
+        });
+    })
+    // catch error if email does not exist
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
+        e,
+      });
+    });
+});
+
+// register endpoint
+app.post("/register", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.passphrase, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        username: request.body.username,
+        passphrase: hashedPassword,
+        publicKey: ChainUtil.genKeyPair().getPublic().encode('hex')
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+//mongoose.connection.once('open', () => {
+  app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+  p2pServer.listen();
+//})
+
--- backend/db/userModel.js
+++ backend/db/userModel.js
@@ -1,21 +1,10 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: 'string',
-        required: [true, "Please provide an username"],
-        unique: [true, "Username exists"]
-    },
-    passphrase: {
-        type: 'string',
-        required: [true, "Please provide a passphrase"],
-    },
-    publicKey: {
-        type: 'string',
-    },
-    privateKey: {
-        type: 'string',
-    },
+const UserSchema = new mongoose.Schema({
+    username: { type: 'string', required: true, unique: true },
+    passphrase: { type: 'string', required: true },
+    publicKey: { type: 'string' },
+    privateKey: { type: 'Buffer'},
 })
 
 module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);

036eaac8d4e56e79ae7aba55506b424745a081c2
Can Dilikoglu <55206720+candilikoglu@users.noreply.github.com>
2022-12-06 13:40:55 -0500

    Mine

--- backend/routers/routers.js
+++ backend/routers/routers.js
@@ -2,9 +2,15 @@ const router = require('express').Router();
 
 const { register, login } = require("../controllers/authControllers")
 const { checkUser } = require("../middlewares/authMiddleware");
+const { mine, mineTransactions } = require("../controllers/miningControllers")
 
+// Authentication routers
 router.post("/", checkUser)
 router.post("/register", register);
 router.post("/login", login);
+
+// Mining routers
+router.get("/mine",  mineTransactions);
+router.post("/mine", mine)
 
 module.exports = router;
